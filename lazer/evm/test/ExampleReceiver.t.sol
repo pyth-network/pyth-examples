295b11d5b1e2722179f8de92553d43e0e0c36942
Pavel Strakhov <ri@idzaaus.org>
2024-11-15 17:43:23 +0000

    feat(lazer): fix evm example tests

--- lazer/evm/test/ExampleReceiver.t.sol
+++ lazer/evm/test/ExampleReceiver.t.sol
@@ -3,13 +3,28 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {ExampleReceiver} from "../src/ExampleReceiver.sol";
+import {PythLazer} from "pyth-lazer/PythLazer.sol";
 
 contract ExampleReceiverTest is Test {
-    ExampleReceiver public receiver;
+    function setUp() public {}
 
-    function setUp() public {
-        receiver = new ExampleReceiver(address(0xb8d50f0bAE75BF6E03c104903d7C3aFc4a6596Da));
+    function test_1() public {
+        address trustedSigner = 0xEfEf56cD66896f6799A90A4e4d512C330c094e44;
+        console.log("trustedSigner %s", trustedSigner);
+
+        PythLazer pythLazer = new PythLazer();
+        pythLazer.initialize(address(0x1));
+        vm.prank(address(0x1));
+        pythLazer.updateTrustedSigner(trustedSigner, 3000000000000000);
+
+        ExampleReceiver receiver = new ExampleReceiver(address(pythLazer));
+
+        bytes memory update =
+            hex"2a22999a577d3cc0202197939d736bc0dcf71b9dde7b9470e4d16fa8e2120c0787a1c0d744d0c39cc372af4d1ecf2d09e84160ca905f3f597d20e2eec144a446a0459ad600001c93c7d3750006240af373971c01010000000201000000000005f5e100";
+        console.logBytes(update);
+
+        receiver.updatePrice(update);
+        assertEq(receiver.price(), 100000000);
+        assertEq(receiver.timestamp(), 1728479312975644);
     }
-
-    function test_1() public {}
 }
