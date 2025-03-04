ec8bc31e49d92b3632e636aa44b20ae9751fd6ac
Aditya Arora <arora.aditya520@gmail.com>
2025-03-04 00:01:52 -0500

    merged into one example

--- price_feeds/evm/chainlink_migration/script/PythAggregatorV3Deployment.s.sol
+++ price_feeds/evm/chainlink_migration/script/PythAggregatorV3Deployment.s.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import {PythAggregatorV3} from "@pythnetwork/pyth-sdk-solidity/PythAggregatorV3.sol";
-import {ChainlinkApp} from "../src/ChainlinkApp.sol";
-
+import "forge-std/console.sol";
 contract PythAggregatorV3Deployment is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -15,16 +14,13 @@ contract PythAggregatorV3Deployment is Script {
         address pythPriceFeedsContract = vm.envAddress("PYTH_ADDRESS");
         // Get the price feed ids from:
         // https://docs.pyth.network/price-feeds/price-feed-ids
-        bytes32 ethFeedId = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
-        bytes32 solFeedId = 0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d;
+        bytes32 priceFeedId = vm.envBytes32("PRICE_FEED_ID");
 
         // Deploy an instance of PythAggregatorV3 for every feed.
         // You can deploy these contracts beforehand if you are integrating with
-        PythAggregatorV3 ethAggregator = new PythAggregatorV3(pythPriceFeedsContract, ethFeedId);
-        PythAggregatorV3 solAggregator = new PythAggregatorV3(pythPriceFeedsContract, solFeedId);
+        PythAggregatorV3 aggregator = new PythAggregatorV3(pythPriceFeedsContract, priceFeedId);
 
-        // Pass the address of the PythAggregatorV3 contract to your chainlink-compatible app.
-        ChainlinkApp app = new ChainlinkApp(address(ethAggregator), address(solAggregator));
+        console.log("PythAggregatorV3 deployed at", address(aggregator));
 
         vm.stopBroadcast();
     }
