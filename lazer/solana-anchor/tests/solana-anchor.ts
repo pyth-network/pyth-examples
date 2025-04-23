9fa402efe343901d972fda66258ac701ac97fd93
Ali Behjati <bahjatia@gmail.com>
2025-04-23 16:35:32 +0200

    chore: add vanilla anchor

---
+++ lazer/solana-anchor/tests/solana-anchor.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SolanaAnchor } from "../target/types/solana_anchor";
+
+describe("solana-anchor", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.SolanaAnchor as Program<SolanaAnchor>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
